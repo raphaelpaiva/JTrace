e6941028646f275a79ae0a985eb94044d852b182
Bruno Costa <doravante2@gmail.com>
2011-12-07 19:15:26 -0200

    Testing notUnit test

--- src/test/org/jtrace/primitives/Vector3DUnitTest.java
+++ src/test/org/jtrace/primitives/Vector3DUnitTest.java
@@ -6,9 +6,9 @@ import org.testng.annotations.Test;
 
 public class Vector3DUnitTest {
 
-	private static final double MODULE_222_VECTOR = Math.sqrt(12);
+	private static final double MODULE_NON_ORIGIN_VECTOR = Math.sqrt(12);
 	
-	private static final double MODULE_000_VECTOR = 0;
+	private static final double MODULE_ORIGIN_VECTOR = 0;
 
 	private static final double PERPENDICULAR_VECTORS_DOT_PRODUCT_RESULT = 0;
 	
@@ -33,25 +33,36 @@ public class Vector3DUnitTest {
 	}
 	
 	@Test
-	public void testModule_Vector_222() {
+	public void testModule_Vector_NonOrigin() {
 		final Vector3D v = new Vector3D(2, 2, 2);
 		
-		Assert.assertEquals(v.module(), MODULE_222_VECTOR);
+		Assert.assertEquals(v.module(), MODULE_NON_ORIGIN_VECTOR);
 	}
 	
 	@Test
-	public void testModule_Vector_000() {
+	public void testModule_Vector_Origin() {
 		final Vector3D v = new Vector3D(0, 0, 0);
 		
-		Assert.assertEquals(v.module(), MODULE_000_VECTOR);
+		Assert.assertEquals(v.module(), MODULE_ORIGIN_VECTOR);
 	}
 	
 	@Test
-	public void testNormalize() {
+	public void testNormalize_UnitVector() {
 		final Vector3D v = new Vector3D(1, 0, 0);
 		final Vector3D normalizedV = v.normal();
 		
 		final Vector3D expected = new Vector3D(1, 0, 0);
+		
+		Assert.assertEquals(normalizedV, expected);
+	}
+	
+	@Test
+	public void testNormalize_NotUnitVector() {
+		final Vector3D v = new Vector3D(2, 3, 4);
+		final Vector3D normalizedV = v.normal();
+		
+		final double module = Math.sqrt(29);
+		final Vector3D expected = new Vector3D(2/module, 3/module, 4/module);
 		
 		Assert.assertEquals(normalizedV, expected);
 	}
