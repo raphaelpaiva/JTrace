3face73dde40d8791c3772a7d8e58660a424fd03
raphaepaiva <raphaeldpaiva@gmail.com>
2011-12-07 23:57:04 -0200

    javadocs for Jay. Closes issue #4

--- src/model/org/jtrace/Jay.java
+++ src/model/org/jtrace/Jay.java
@@ -3,17 +3,31 @@ package org.jtrace;
 import org.jtrace.primitives.Point3D;
 import org.jtrace.primitives.Vector3D;
 
+/**
+ * Basic Class representing a casted Ray.
+ * 
+ * @author raphaelpaiva
+ * @author brunocosta
+ * @author flaviocdc
+ *
+ */
 public class Jay {
-	private final Point3D point;
+	private final Point3D origin;
 	private final Vector3D direction;
 
-	public Jay(final Point3D point, final Vector3D direction) {
-		this.point = point;
+	/**
+	 * Creates a {@link Jay} from its origin coordinates and a {@link Vector3D} indicating its direction.
+	 * 
+	 * @param origin a {@link Point3D} representing the origin coordinates of the {@link Jay}.
+	 * @param direction a {@link Vector3D} representing the {@link Jay} direction.
+	 */
+	public Jay(final Point3D origin, final Vector3D direction) {
+		this.origin = origin;
 		this.direction = direction;
 	}
 
 	public Point3D getPoint() {
-		return point;
+		return origin;
 	}
 
 	public Vector3D getDirection() {
