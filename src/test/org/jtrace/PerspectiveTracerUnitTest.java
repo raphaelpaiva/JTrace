425671fcb283a4f922eb74023d02c984d814deed
Bruno Costa <brunocosta@chronos>
2011-12-19 13:03:30 -0200

    Changing tests

--- src/test/org/jtrace/PerspectiveTracerUnitTest.java
+++ src/test/org/jtrace/PerspectiveTracerUnitTest.java
@@ -1,6 +1,5 @@
 package org.jtrace;
 
-import org.jtrace.geometry.Plane;
 import org.jtrace.geometry.Sphere;
 import org.jtrace.primitives.ColorRGB;
 import org.jtrace.primitives.Point3D;
@@ -60,56 +59,19 @@ public class PerspectiveTracerUnitTest {
 		Assert.assertEquals(TRACER.cast(scene, jay), ColorRGB.RED);
 	}
 	
-	/*
 	@Test
 	public void testRender_RedSphereBehindViewPlane() {
 		final Point3D center = new Point3D(0, 0, 50);
 		final Sphere sphere = new Sphere(center, SPHERE_RADIUS, ColorRGB.RED);
+		final int hres = 1;
+		final int vres = 1;
+		final double pixelSize = 0.5;
 		
 		Scene scene = new Scene().add(sphere).withBackground(ColorRGB.GREEN);
 		
-		Assert.assertEquals(TRACER.cast(scene, JAY), ColorRGB.GREEN);
+		Vector3D jayDirection = TRACER.calculateJayDirection(hres, vres, pixelSize, 0, 0);
+		Jay jay = new Jay(EYE_POINT, jayDirection);
+		
+		Assert.assertEquals(TRACER.cast(scene, jay), ColorRGB.GREEN);
 	}
-	
-	@Test
-	public void testRender_TwoSpheresRedInFront() {
-		final Point3D centerRed = new Point3D(0, 0, -10);
-		final Point3D centerBlue = new Point3D(0, 0, -20);
-		
-		final Sphere red = new Sphere(centerRed, SPHERE_RADIUS, ColorRGB.RED);
-		final Sphere blue = new Sphere(centerBlue, SPHERE_RADIUS, ColorRGB.BLUE);
-		
-		Scene scene = new Scene().add(blue, red);
-		
-		Assert.assertEquals(TRACER.cast(scene, JAY), ColorRGB.RED);
-		
-		scene = new Scene().add(red, blue);
-		
-		Assert.assertEquals(TRACER.cast(scene, JAY), ColorRGB.RED);
-	}
-	
-	@Test
-	public void testRender_PlaneInFrontOfViewPlane() {
-		final Point3D point = new Point3D(0, 0, -5);
-		final Vector3D normal = new Vector3D(0, 0, 1);
-		
-		final Plane plane = new Plane(point, normal, ColorRGB.YELLOW);
-		
-		Scene scene = new Scene().add(plane);
-
-		Assert.assertEquals(TRACER.cast(scene, JAY), ColorRGB.YELLOW);
-	}
-	
-	@Test
-	public void testRender_PlaneBehindViewPlane() {
-		final Point3D point = new Point3D(0, 0, 5);
-		final Vector3D normal = new Vector3D(0, 0, 1);
-		
-		final Plane plane = new Plane(point, normal, ColorRGB.YELLOW);
-		
-		Scene scene = new Scene().add(plane);
-
-		Assert.assertEquals(TRACER.cast(scene, JAY), ColorRGB.BLACK);
-	}
-	*/
 }
