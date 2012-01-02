7b264d2fd2a1520cb553a944f8d007b8433dffe8
Raphael Paiva <raphaeldpaiva@gmail.com>
2012-01-02 18:42:59 -0200

    adding the white colorwq

--- src/model/org/jtrace/primitives/ColorRGB.java
+++ src/model/org/jtrace/primitives/ColorRGB.java
@@ -8,16 +8,17 @@ package org.jtrace.primitives;
  *
  */
 public class ColorRGB {
-	private static double MAX_COLOR_VALUE = 1;
-	private static double MIN_COLOR_VALUE = 0;
-	
-	public static ColorRGB BLACK   = new ColorRGB(0.0, 0.0, 0.0);
-	public static ColorRGB RED     = new ColorRGB(1.0, 0.0, 0.0);
-	public static ColorRGB GREEN   = new ColorRGB(0.0, 1.0, 0.0);
-	public static ColorRGB BLUE    = new ColorRGB(0.0, 0.0, 1.0);
-	public static ColorRGB YELLOW  = new ColorRGB(1.0, 1.0, 0.0);
-	public static ColorRGB PURPLE  = new ColorRGB(0.5, 0.0, 0.5);
-	
+    private static double MAX_COLOR_VALUE = 1;
+    private static double MIN_COLOR_VALUE = 0;
+
+    public static ColorRGB BLACK   = new ColorRGB(0.0, 0.0, 0.0);
+    public static ColorRGB RED     = new ColorRGB(1.0, 0.0, 0.0);
+    public static ColorRGB GREEN   = new ColorRGB(0.0, 1.0, 0.0);
+    public static ColorRGB BLUE    = new ColorRGB(0.0, 0.0, 1.0);
+    public static ColorRGB YELLOW  = new ColorRGB(1.0, 1.0, 0.0);
+    public static ColorRGB PURPLE  = new ColorRGB(0.5, 0.0, 0.5);
+    public static ColorRGB WHITE   = new ColorRGB(1.0, 1.0, 1.0);
+
     private double red, green, blue;
 
     /**
@@ -32,30 +33,30 @@ public class ColorRGB {
         setGreen(paramG);
         setBlue(paramB);
     }
-    
+
     public int toInt() {
-    	long rgb = Math.round(red * 255);
-    	rgb = (rgb << 8) + Math.round(green * 255);
-    	rgb = (rgb << 8) + Math.round(blue * 255);
-    	
-    	return (int) rgb;
-    }
-    
-    public ColorRGB add(ColorRGB color2) {
-    	return new ColorRGB(this.getRed() + color2.getRed(), this.getGreen() + color2.getGreen(), this.getBlue() + color2.getBlue());
+        long rgb = Math.round(red * 255);
+        rgb = (rgb << 8) + Math.round(green * 255);
+        rgb = (rgb << 8) + Math.round(blue * 255);
+
+        return (int) rgb;
     }
 
-    public ColorRGB multiply(float multiplier) {
-    	return new ColorRGB(this.getRed() * multiplier, this.getGreen() * multiplier, this.getBlue() * multiplier);
+    public ColorRGB add(final ColorRGB color2) {
+        return new ColorRGB(getRed() + color2.getRed(), getGreen() + color2.getGreen(), getBlue() + color2.getBlue());
     }
-    
+
+    public ColorRGB multiply(final float multiplier) {
+        return new ColorRGB(getRed() * multiplier, getGreen() * multiplier, getBlue() * multiplier);
+    }
+
     public double getRed() {
         return red;
     }
 
     public void setRed(final double r) {
-        this.red = Math.min(MAX_COLOR_VALUE, r);
-        this.red = Math.max(MIN_COLOR_VALUE, this.red);
+        red = Math.min(MAX_COLOR_VALUE, r);
+        red = Math.max(MIN_COLOR_VALUE, red);
     }
 
     public double getGreen() {
@@ -63,8 +64,8 @@ public class ColorRGB {
     }
 
     public void setGreen(final double g) {
-        this.green = Math.min(MAX_COLOR_VALUE, g);
-        this.green = Math.max(MIN_COLOR_VALUE, this.green);
+        green = Math.min(MAX_COLOR_VALUE, g);
+        green = Math.max(MIN_COLOR_VALUE, green);
     }
 
     public double getBlue() {
@@ -72,45 +73,51 @@ public class ColorRGB {
     }
 
     public void setBlue(final double b) {
-        this.blue = Math.min(MAX_COLOR_VALUE, b);
-        this.blue = Math.max(MIN_COLOR_VALUE, this.blue);
+        blue = Math.min(MAX_COLOR_VALUE, b);
+        blue = Math.max(MIN_COLOR_VALUE, blue);
     }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		long temp;
-		temp = Double.doubleToLongBits(blue);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		temp = Double.doubleToLongBits(green);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		temp = Double.doubleToLongBits(red);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        long temp;
+        temp = Double.doubleToLongBits(blue);
+        result = prime * result + (int) (temp ^ temp >>> 32);
+        temp = Double.doubleToLongBits(green);
+        result = prime * result + (int) (temp ^ temp >>> 32);
+        temp = Double.doubleToLongBits(red);
+        result = prime * result + (int) (temp ^ temp >>> 32);
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ColorRGB other = (ColorRGB) obj;
-		if (Double.doubleToLongBits(blue) != Double.doubleToLongBits(other.blue))
-			return false;
-		if (Double.doubleToLongBits(green) != Double.doubleToLongBits(other.green))
-			return false;
-		if (Double.doubleToLongBits(red) != Double.doubleToLongBits(other.red))
-			return false;
-		return true;
-	}
-	
-	@Override
-	public String toString() {
-		return "Color(" + red + ", " + green + ", " + blue + ")";
-	}
-    
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ColorRGB other = (ColorRGB) obj;
+        if (Double.doubleToLongBits(blue) != Double.doubleToLongBits(other.blue)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(green) != Double.doubleToLongBits(other.green)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(red) != Double.doubleToLongBits(other.red)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Color(" + red + ", " + green + ", " + blue + ")";
+    }
+
 }
