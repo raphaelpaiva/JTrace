a37d0073f647d927ad6b2753e99211c13a28dcbf
Raphael Paiva <raphaeldpaiva@gmail.com>
2012-01-02 17:52:21 -0200

    Creating the ORIGIN point.

--- src/model/org/jtrace/primitives/Point3D.java
+++ src/model/org/jtrace/primitives/Point3D.java
@@ -9,96 +9,104 @@ package org.jtrace.primitives;
  *
  */
 public class Point3D {
-    private double x, y, z;
+	public static final Point3D ORIGIN = new Point3D(0, 0, 0);
 
-    /**
-     * Create a point from its coordinates
-     *  
-     * @param x axis value
-     * @param y axis value
-     * @param z axis value
-     */
-    public Point3D(final double x, final double y, final double z) {
-        super();
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-    
-    /**
-     * Subtract operation
-     * 
-     * Performs the basic point subtraction.<br>
-     * Suppose the points A(x, y, z) and B(u, v, w).<br>
-     * 
-     * A - B = C(x - u, y - v, z - w)
-     * 
-     * @param otherVector the point to subtract.
-     * @return a new {@link Point3D} equivalent to <code>(this - otherVector)</code>
-     */
-    public Point3D subtract(Point3D otherVector) {
-        return new Point3D(x - otherVector.getX(), y - otherVector.getY(), z - otherVector.getZ());
-    }
+	private double x, y, z;
 
-    public double getX() {
-        return x;
-    }
+	/**
+	 * Create a point from its coordinates
+	 * 
+	 * @param x axis value
+	 * @param y axis value
+	 * @param z axis value
+	 */
+	public Point3D(final double x, final double y, final double z) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
 
-    public void setX(final double x) {
-        this.x = x;
-    }
+	/**
+	 * Subtract operation
+	 * 
+	 * Performs the basic point subtraction.<br>
+	 * Suppose the points A(x, y, z) and B(u, v, w).<br>
+	 * 
+	 * A - B = C(x - u, y - v, z - w)
+	 * 
+	 * @param otherVector the point to subtract.
+	 * @return a new {@link Point3D} equivalent to <code>(this - otherVector)</code>
+	 */
+	public Point3D subtract(final Point3D otherVector) {
+		return new Point3D(x - otherVector.getX(), y - otherVector.getY(), z - otherVector.getZ());
+	}
 
-    public double getY() {
-        return y;
-    }
+	public double getX() {
+		return x;
+	}
 
-    public void setY(final double y) {
-        this.y = y;
-    }
+	public void setX(final double x) {
+		this.x = x;
+	}
 
-    public double getZ() {
-        return z;
-    }
+	public double getY() {
+		return y;
+	}
 
-    public void setZ(final double z) {
-        this.z = z;
-    }
+	public void setY(final double y) {
+		this.y = y;
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        long temp;
-        temp = Double.doubleToLongBits(x);
-        result = prime * result + (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(y);
-        result = prime * result + (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(z);
-        result = prime * result + (int) (temp ^ (temp >>> 32));
-        return result;
-    }
+	public double getZ() {
+		return z;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Point3D other = (Point3D) obj;
-        if (x != other.x)
-            return false;
-        if (y != other.y)
-            return false;
-        if (z != other.z)
-            return false;
-        return true;
-    }
+	public void setZ(final double z) {
+		this.z = z;
+	}
 
-    @Override
-    public String toString() {
-    	return "(" + x + ", " + y + ", " + z + ")";
-    }
-    
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(x);
+		result = prime * result + (int) (temp ^ temp >>> 32);
+		temp = Double.doubleToLongBits(y);
+		result = prime * result + (int) (temp ^ temp >>> 32);
+		temp = Double.doubleToLongBits(z);
+		result = prime * result + (int) (temp ^ temp >>> 32);
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Point3D other = (Point3D) obj;
+		if (x != other.x) {
+			return false;
+		}
+		if (y != other.y) {
+			return false;
+		}
+		if (z != other.z) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + x + ", " + y + ", " + z + ")";
+	}
+
 }
