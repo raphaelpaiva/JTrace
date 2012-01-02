6b785856ac3b0129cc755183ef471eac566a006d
Raphael Paiva <raphaeldpaiva@gmail.com>
2012-01-02 17:44:05 -0200

    Creating the Materials class. Closes #11

---
+++ src/model/org/jtrace/Materials.java
@@ -0,0 +1,15 @@
+package org.jtrace;
+
+import org.jtrace.primitives.ColorRGB;
+import org.jtrace.primitives.ReflectanceCoefficient;
+
+public class Materials {
+
+	public static Material matte(final ColorRGB color) {
+		final ReflectanceCoefficient kAmbient = new ReflectanceCoefficient(0.07, 0.07, 0.07);
+		final ReflectanceCoefficient kDiffuse = new ReflectanceCoefficient(0.3, 0.3, 0.3);
+
+		return new Material(color, kAmbient, kDiffuse);
+	}
+
+}
