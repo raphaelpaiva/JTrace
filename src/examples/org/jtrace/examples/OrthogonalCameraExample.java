d73375bd065e6b917c266e5bcbaaf72480d5265e
Raphael Paiva <raphaeldpaiva@gmail.com>
2012-01-10 16:37:45 -0200

    Removing throws clause form main method

--- src/examples/org/jtrace/examples/OrthogonalCameraExample.java
+++ src/examples/org/jtrace/examples/OrthogonalCameraExample.java
@@ -1,7 +1,5 @@
 package org.jtrace.examples;
 
-import java.io.IOException;
-
 import org.jtrace.Materials;
 import org.jtrace.Scene;
 import org.jtrace.Tracer;
@@ -17,29 +15,29 @@ import org.jtrace.primitives.Point3D;
 import org.jtrace.primitives.Vector3D;
 
 public class OrthogonalCameraExample {
-  public static void main(String[] args) throws IOException {
-    ViewPlane viewPlane = new ViewPlane(1024, 768);
+    public static void main(final String[] args) {
+        final ViewPlane viewPlane = new ViewPlane(1024, 768);
 
-    Point3D eye = new Point3D(0, 0, 200);
-    Point3D lookAt = new Point3D(0, 0, 100);
-    Vector3D up = new Vector3D(0, 1, 0);
-    
-    Camera camera = new OrthogonalCamera(eye, lookAt, up);
-    
-    Light light = new Light(-100, -50, -10);
-    
-    final Point3D centerRed = new Point3D(-50, 0, -10);
-    final Point3D centerBlue = new Point3D(50, 0, -100);
+        final Point3D eye = new Point3D(0, 0, 200);
+        final Point3D lookAt = new Point3D(0, 0, 100);
+        final Vector3D up = new Vector3D(0, 1, 0);
 
-    final Sphere red = new Sphere(centerRed, 50, Materials.matte(ColorRGB.RED));
-    final Sphere blue = new Sphere(centerBlue, 50, Materials.matte(ColorRGB.BLUE));
+        final Camera camera = new OrthogonalCamera(eye, lookAt, up);
 
-    Scene scene = new Scene().add(blue, red).add(light).setCamera(camera);
+        final Light light = new Light(-100, -50, -10);
 
-    Tracer ot = new Tracer();
+        final Point3D centerRed = new Point3D(-50, 0, -10);
+        final Point3D centerBlue = new Point3D(50, 0, -100);
 
-    ot.addListeners(new ImageListener("result.png", "png"), new TimeListener());
+        final Sphere red = new Sphere(centerRed, 50, Materials.matte(ColorRGB.RED));
+        final Sphere blue = new Sphere(centerBlue, 50, Materials.matte(ColorRGB.BLUE));
 
-    ot.render(scene, viewPlane);
-  }
+        final Scene scene = new Scene().add(blue, red).add(light).setCamera(camera);
+
+        final Tracer ot = new Tracer();
+
+        ot.addListeners(new ImageListener("result.png", "png"), new TimeListener());
+
+        ot.render(scene, viewPlane);
+    }
 }
