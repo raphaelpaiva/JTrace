e6a2ae8d03db2c084fa7c21ac1ebe415cec69598
Flavio Costa <flavio.cdc@gmail.com>
2012-01-10 02:13:39 -0200

    Adding private default constructor and equals/hashCode

--- src/model/org/jtrace/primitives/ReflectanceCoefficient.java
+++ src/model/org/jtrace/primitives/ReflectanceCoefficient.java
@@ -8,7 +8,11 @@ public class ReflectanceCoefficient {
 	
 	private double blue;
 
+	private ReflectanceCoefficient() { }
+	
 	public ReflectanceCoefficient(double red, double green, double blue) {
+		this();
+		
 		this.red = red;
 		this.green = green;
 		this.blue = blue;
@@ -25,5 +29,39 @@ public class ReflectanceCoefficient {
 	public double getBlue() {
 		return blue;
 	}
+
+	@Override
+  public int hashCode() {
+	  final int prime = 31;
+	  int result = 1;
+	  long temp;
+	  temp = Double.doubleToLongBits(blue);
+	  result = prime * result + (int) (temp ^ (temp >>> 32));
+	  temp = Double.doubleToLongBits(green);
+	  result = prime * result + (int) (temp ^ (temp >>> 32));
+	  temp = Double.doubleToLongBits(red);
+	  result = prime * result + (int) (temp ^ (temp >>> 32));
+	  return result;
+  }
+
+	@Override
+  public boolean equals(Object obj) {
+	  if (this == obj)
+		  return true;
+	  if (obj == null)
+		  return false;
+	  if (getClass() != obj.getClass())
+		  return false;
+	  ReflectanceCoefficient other = (ReflectanceCoefficient) obj;
+	  if (Double.doubleToLongBits(blue) != Double.doubleToLongBits(other.blue))
+		  return false;
+	  if (Double.doubleToLongBits(green) != Double.doubleToLongBits(other.green))
+		  return false;
+	  if (Double.doubleToLongBits(red) != Double.doubleToLongBits(other.red))
+		  return false;
+	  return true;
+  }
+	
+	
 	
 }
