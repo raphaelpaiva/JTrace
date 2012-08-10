97443b68c672cafcea85e573749fbcaa5c08698d
Raphael Paiva <raphaelpaiva@gmail.com>
2012-08-10 17:19:28 -0300

    Support classes for SpecularShader. Test Needed.

--- src/model/org/jtrace/Material.java
+++ src/model/org/jtrace/Material.java
@@ -10,11 +10,20 @@ public class Material {
 	private ReflectanceCoefficient kAmbient;
 	
 	private ReflectanceCoefficient kDiffuse;
+	
+	private ReflectanceCoefficient kSpecular;
 
 	public Material(ColorRGB color, ReflectanceCoefficient kAmbient, ReflectanceCoefficient kDiffuse) {
 		this.color = color;
 		this.kAmbient = kAmbient;
 		this.kDiffuse = kDiffuse;
+	}
+	
+	public Material(ColorRGB color, ReflectanceCoefficient kAmbient, ReflectanceCoefficient kDiffuse, ReflectanceCoefficient kSpecular) {
+		this.color = color;
+		this.kAmbient = kAmbient;
+		this.kDiffuse = kDiffuse;
+		this.kSpecular = kSpecular;
 	}
 
 	public ColorRGB getColor() {
@@ -27,6 +36,14 @@ public class Material {
 
 	public ReflectanceCoefficient getkDiffuse() {
 		return kDiffuse;
+	}
+
+	public ReflectanceCoefficient getkSpecular() {
+		return kSpecular;
+	}
+
+	public void setkSpecular(ReflectanceCoefficient kSpecular) {
+		this.kSpecular = kSpecular;
 	}
 	
 }
--- src/model/org/jtrace/Materials.java
+++ src/model/org/jtrace/Materials.java
@@ -11,5 +11,13 @@ public class Materials {
 
 		return new Material(color, kAmbient, kDiffuse);
 	}
+	
+	public static Material metallic(final ColorRGB color) {
+		final ReflectanceCoefficient kAmbient = new ReflectanceCoefficient(0.07, 0.07, 0.07);
+		final ReflectanceCoefficient kDiffuse = new ReflectanceCoefficient(0.3, 0.3, 0.3);
+		final ReflectanceCoefficient kSpecular = new ReflectanceCoefficient(0.2, 0.2, 0.2);
+
+		return new Material(color, kAmbient, kDiffuse, kSpecular);
+	}
 
 }
