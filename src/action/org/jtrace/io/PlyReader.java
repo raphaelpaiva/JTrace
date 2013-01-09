9efb33f56a8cc52e7d0b0787497e4fc46221bfab
Raphael Paiva <raphaeldpaiva@gmail.com>
2013-01-09 19:47:23 -0200

    Reading Triangles Meshes from PLY files!

---
+++ src/action/org/jtrace/io/PlyReader.java
@@ -0,0 +1,89 @@
+package org.jtrace.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jtrace.Material;
+import org.jtrace.Materials;
+import org.jtrace.geometry.Triangle;
+import org.jtrace.primitives.ColorRGB;
+import org.jtrace.primitives.Point3D;
+import org.smurn.jply.Element;
+import org.smurn.jply.ElementReader;
+import org.smurn.jply.PlyReaderFile;
+import org.smurn.jply.util.NormalMode;
+import org.smurn.jply.util.NormalizingPlyReader;
+import org.smurn.jply.util.TesselationMode;
+import org.smurn.jply.util.TextureMode;
+
+public class PlyReader {
+
+	public static List<Triangle> read(InputStream is, Material material) throws IOException {
+		
+		org.smurn.jply.PlyReader plyReader = new PlyReaderFile(is);
+		
+		plyReader = new NormalizingPlyReader(plyReader, TesselationMode.TRIANGLES,
+														NormalMode.PASS_THROUGH,
+														TextureMode.PASS_THROUGH);
+		
+		ElementReader elementReader = plyReader.nextElementReader();
+		
+		List<Point3D>  vertices  = null;
+		List<Triangle> triangles = null;
+		
+		while (elementReader != null) {
+			if (elementReader.getElementType().getName().equals("vertex")) {
+				vertices = readVertices(elementReader);
+			}
+			
+			if (elementReader.getElementType().getName().equals("face")) {
+				triangles = readTriangles(elementReader, vertices, material);
+			}
+			
+			elementReader = plyReader.nextElementReader();
+		}
+		
+		return triangles;
+	}
+ 
+	private static List<Triangle> readTriangles(ElementReader elementReader, List<Point3D> vertices, Material material) throws IOException {
+		Element faceElement = elementReader.readElement();
+		
+		List<Triangle> triangles = new ArrayList<Triangle>(elementReader.getCount());
+		
+		while (faceElement != null) {
+			int[] indexes = faceElement.getIntList("vertex_index");
+			
+			if (indexes.length == 3) {
+				int a = indexes[0];
+				int b = indexes[1];
+				int c = indexes[2];
+				
+				triangles.add(new Triangle(vertices.get(a), vertices.get(b), vertices.get(c), material));
+			}
+			
+			faceElement = elementReader.readElement();
+		}
+		
+		return triangles;
+	}
+ 
+	private static List<Point3D> readVertices(ElementReader elementReader) throws IOException {
+		List<Point3D> vertices = new ArrayList<Point3D>(elementReader.getCount());
+		
+		Element vertexElement = elementReader.readElement();
+		
+		while (vertexElement != null) {
+			
+			Point3D vertex = new Point3D(vertexElement.getDouble("x"), vertexElement.getDouble("y"), vertexElement.getDouble("z"));
+			
+			vertices.add(vertex);
+			
+			vertexElement = elementReader.readElement();
+		}
+		
+		return vertices;
+	}
+}
---
+++ src/examples/org/jtrace/examples/io/SimplePLYExample.java
@@ -0,0 +1,55 @@
+package org.jtrace.examples.io;
+
+import java.awt.Toolkit;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.jtrace.Materials;
+import org.jtrace.Scene;
+import org.jtrace.Tracer;
+import org.jtrace.ViewPlane;
+import org.jtrace.cameras.Camera;
+import org.jtrace.cameras.PinHoleCamera;
+import org.jtrace.geometry.Triangle;
+import org.jtrace.io.PlyReader;
+import org.jtrace.lights.Light;
+import org.jtrace.listeners.ImageListener;
+import org.jtrace.listeners.TimeListener;
+import org.jtrace.primitives.ColorRGB;
+import org.jtrace.primitives.Point3D;
+import org.jtrace.primitives.Vector3D;
+import org.jtrace.shader.Shaders;
+
+public class SimplePLYExample {
+	public static void main(String[] args) throws IOException {
+				
+		InputStream is = SimplePLYExample.class.getResourceAsStream("simple.ply");
+		List<Triangle> triangles;
+ 
+		triangles = PlyReader.read(is, Materials.metallic(ColorRGB.WHITE));
+		Scene scene = new Scene();
+		
+		for (Triangle t : triangles) {
+			scene.add(t);
+		}
+		
+		scene.add(new Light(0, 0, 5));
+		
+		Camera camera = new PinHoleCamera(new Point3D(0, 0, 10), Point3D.ORIGIN, Vector3D.UNIT_Y);
+		
+		camera.setZoomFactor(10);
+		
+		scene.setCamera(camera);
+		
+		Tracer tracer = new Tracer();
+		
+		tracer.addShaders(Shaders.ambientShader(), Shaders.diffuseShader(), Shaders.specularShader(64));
+		
+		tracer.addListeners(new ImageListener("simple.png", "png"), new TimeListener());
+		
+		tracer.render(scene, new ViewPlane(1280, 768));
+		
+		Toolkit.getDefaultToolkit().beep();
+	}
+}
