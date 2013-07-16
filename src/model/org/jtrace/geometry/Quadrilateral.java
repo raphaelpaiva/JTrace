369dc17dcafe0c5b58b975e461b9e1cbc5730917
Raphael Paiva <raphaeldpaiva@gmail.com>
2013-07-16 11:33:28 -0300

    Adding Javadoc to Quadrilateral Class.

--- src/model/org/jtrace/geometry/Quadrilateral.java
+++ src/model/org/jtrace/geometry/Quadrilateral.java
@@ -9,11 +9,30 @@ import org.jtrace.NotHit;
 import org.jtrace.Section;
 import org.jtrace.primitives.Point3D;
 
+/**
+ * Represents a quadrilateral in three-dimensional space.
+ * 
+ * A quadrilateral is not necessarily planar and is formed by two
+ * {@link Triangle} instances.
+ * 
+ * @author raphaelpaiva
+ * @author ethiago
+ */
 public class Quadrilateral extends GeometricObject {
 
 	private Triangle t1;
 	private Triangle t2;
 	
+	/**
+	 * Creates a {@link Quadrilateral} given it's four edges in
+	 * counter-clockwise order.
+	 * 
+	 * @param p0 the first edge in counter-clockwise order
+	 * @param p1 the second edge in counter-clockwise order
+	 * @param p2 the third edge in counter-clockwise order
+	 * @param p3 the fourth edge in counter-clockwise order
+	 * @param material the {@link Quadrilateral}'s {@link Material}.
+	 */
 	public Quadrilateral(Point3D p0, Point3D p1, Point3D p2, Point3D p3, Material material) {
 		super(material);
 		
